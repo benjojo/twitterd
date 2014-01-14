eb118804fd172fd2e2a62ebcb62df5d9d2b0b755
Ben Cox <Ben@Benjojo.co.uk>
2014-01-14 19:36:06 +0000

    Added base that I will start off with.

--- main.go
+++ main.go
@@ -2,8 +2,52 @@ package main
 
 import (
 	"fmt"
+	"github.com/ChimeraCoder/anaconda"
+	"github.com/darkhelmet/twitterstream"
+	"io/ioutil"
+	"log"
+	"net/url"
+	"strings"
 )
 
 func main() {
 	fmt.Println("hi")
+	b, e := ioutil.ReadFile("./twittercfg")
+	if e != nil {
+		log.Fatal("Could not read the ./twittercfg file.")
+	}
+	twittertemp := string(b)
+	twitterbits := strings.Split(twittertemp, "\n")
+	if len(twitterbits) != 5 {
+		log.Fatal("Not enought things in twitter cfg, Needs to be (seperated by \\n) username, consumerKey, consumerSecret, accessToken, accessSecret")
+	}
+	Client := twitterstream.NewClient(twitterbits[1], twitterbits[2], twitterbits[3], twitterbits[4])
+	Conn, e := Client.Track(fmt.Sprintf("@%s", twitterbits[0]))
+	// Streamign API is setup now, now just setup the general purpose one now
+	anaconda.SetConsumerKey(twitterbits[1])
+	anaconda.SetConsumerSecret(twitterbits[2])
+	api := anaconda.NewTwitterApi(twitterbits[3], twitterbits[4])
+
+	if e != nil {
+		log.Fatal("could not open a streaming connection to get mentions :(")
+	}
+	for {
+		t, e := Conn.Next()
+		if e == nil {
+			fmt.Printf("TWEET: %s\n", t.Text)
+			fmt.Printf("OWNER @%s\n", strings.ToLower(twitterbits[0]))
+			if strings.HasPrefix(strings.ToLower(t.Text), fmt.Sprintf("@%s", strings.ToLower(twitterbits[0]))) {
+				v := url.Values{} // I dont even know
+				t, e := api.PostTweet(fmt.Sprintf("@%s pong", t.User.ScreenName), v)
+				if e == nil {
+					fmt.Println(t)
+				} else {
+					fmt.Println(e)
+				}
+			} else {
+				fmt.Println("Does not start with @<user> ignoring")
+			}
+		}
+	}
+
 }
