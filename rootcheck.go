ee0f949c98a102dee26fa4aae0283ceee98f6703
Ben Cox <ben@benjojo.co.uk>
2014-02-25 09:17:11 +0000

    Changed the root checking dialog kinder
    And much less obnoxious

--- rootcheck.go
+++ rootcheck.go
@@ -9,10 +9,11 @@ func CheckIfUserIsRoot() {
 	CurrentUsr, _ := user.Current()
 	if CurrentUsr.Uid == "0" {
 		for i := 0; i < 10; i++ {
+			Hashes := ""
 			for hashes := 0; hashes < i; hashes++ {
-				Logger.Print("#")
+				Hashes = Hashes + "#"
 			}
-			Logger.Println("!!WARNING!! YOU ARE RUNNING THIS AS ROOT, THIS ISNT A GOOD IDEA")
+			Logger.Println(Hashes, "!!WARNING!! YOU ARE RUNNING THIS AS ROOT, THIS ISNT A GOOD IDEA")
 		}
 		Logger.Println("pausing for 5 seconds because of this.")
 		time.Sleep(time.Second * 5)
